707efb643648eb06b767296838325253264547c4
Muncherxyz <101988752+Muncherxyz@users.noreply.github.com>
2023-10-10 12:44:10 -0500

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""this model kills facists"""
-
+"""
+    creates an instance of FileStorage
+"""
 from models.engine.file_storage import FileStorage
+from models import base_model
+from models.base_model import BaseModel
 
 storage = FileStorage()
 storage.reload()
